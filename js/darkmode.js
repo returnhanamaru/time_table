c771574c96d2c8017dc7aa9e7cb67bda5a60427a
returnhanamaru <xenon54.48@gmail.com>
2025-05-07 14:25:29 +0900

    カレンダーと表の位置を調整

--- js/darkmode.js
+++ js/darkmode.js
@@ -7,8 +7,8 @@ const options = {
    backgroundColor: "#fff", // default: '#fff'
    buttonColorDark: "#100f2c", // default: '#100f2c'
    buttonColorLight: "#fff", // default: '#fff'
-   saveInCookies: false, // default: true,
-   label: "暗", // default: ''
+   saveInCookies: true, // default: true,
+   label: "", // default: ''
    autoMatchOsTheme: true, // default: true
 };
 
